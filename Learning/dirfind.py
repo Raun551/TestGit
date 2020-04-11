d37c918a982477d13ae7c1e8002e001c92d1a076
Raunaq Malik <raunaq.malik23@gmail.com>
2020-04-11 14:44:32 +0100

    os python

---
+++ Learning/dirfind.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os
+dir = "/home/raunaq.malik"
+for name in os.listdir(dir):
+    fullname = os.path.join(dir, name)
+    if os.path.isdir(fullname):
+        print("{} is a directory".format(fullname))
+    else:
+        print("{} is a file".format(fullname))
